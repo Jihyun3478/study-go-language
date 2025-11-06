b2ad1fbfed582ce8f83002bcb9d62a006edba619
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-06 17:38:57 +0900

    feat: naked return 실습
    naked return 실습
    - 리턴값을 상단에 명시해주면, naked return 적용이 가능하다.

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string){
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("nico", "lynn", "dal", "flynn", "muzi")
+	totalLength, up :=lenAndUpper("jihyun")
+	fmt.Println(totalLength, up)
 }
